d828e0fb60f2099ae186b4c55df756acab1d5f3e
cwjpalmer <cwjpalmer@gmail.com>
2015-04-16 21:27:47 +0200

    fixed broken variable names, tested, works as desired

--- Box_modules/Module_Digital_Pet/Pet_alive_code.ino
+++ Box_modules/Module_Digital_Pet/Pet_alive_code.ino
@@ -1,41 +1,41 @@
 int ledPin1 = 10;
 int ledPin2 = 11;
- 
-int sensor pin = 0;
-int sensor threshold = 20;
- 
+
+int sensorPin = A0;
+int threshold = 20;
+
 int speakerPin = 9;
- 
-int light normal; 
-  
+
+int lightNormal;
+
 void setup () {
   pinMode (ledPin1, OUTPUT );
   pinMode (ledPin2, OUTPUT );
-   
-  light = normal analog read (have sensor);
+
+  lightNormal = analogRead (sensorPin);
 }
-  
+
 void loop () {
   // Switch LEDs for eyes
   digitalWrite (ledPin1, HIGH );
   digitalWrite (ledPin2, HIGH );
-   
+
   // Wink
   if ( random (500) == 1) {
     digitalWrite (ledPin1, LOW );
     digitalWrite (ledPin2, LOW );
     delay (50);
   }
-   
+
   // Purr
-  if ( analog read (have sensor)> light sensor normal + threshold) {
+  if ( analogRead (sensorPin) < lightNormal - threshold) {
     tone (speakerPin, 0001);
   } else {
     noTone (speakerPin);
   }
-   
+
   // Alive
-  if (( random (10000) == 1) && ( analog read (have sensor) <normal light + 100)) {
+  if (( random (10000) == 1) && ( analogRead (sensorPin) < lightNormal + 100)) {
     tone (speakerPin, 1200);
     delay (100);
     tone (speakerPin, 800);
@@ -44,7 +44,7 @@ void loop () {
     delay (200);
     tone (speakerPin, 1200);
     delay (100);
-  } 
-   
+  }
+
   delay (10);
 }
